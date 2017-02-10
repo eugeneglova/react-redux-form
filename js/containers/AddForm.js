942b969b3a1f4c6353b64e7c384186bcf6e2b737
Eugene Glova <eugene.glova@gmail.com>
2017-02-10 19:40:12 +0300

    Use table

--- js/containers/AddForm.js
+++ js/containers/AddForm.js
@@ -8,69 +8,83 @@ import Button from '../components/Button';
 const AddForm = connect()(
 	() => (
 		<div>
-			<div>
-				<div>
-					<Label text="Title" required={true} />
-					<Input />
-				</div>
-				<div>
-					<Label text="From" required={true} />
-					<Input /> : <Input />
-				</div>
-				<div>
-					<Label text="To" required={true} />
-					<Input /> : <Input />
-				</div>
-				<div>
-					<label>
-						<input type="checkbox" />
-						Run All Day
-					</label>
-				</div>
-				<div>
-					<Label text="Type" required={true} />
-					<SelectMultiple
-						title="Please select [Type]"
-						data={['All', 'Deposits', 'Withdrawals']}
-					/>
-				</div>
-				<div>
-					<Label text="Day" required={true} />
-					<SelectMultiple
-						title="Please select [Day]"
-						data={['All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']}
-					/>
-				</div>
-				<div>
-					<Label text="Min Amount" />
-					<Input />
-				</div>
-				<div>
-					<Label text="Max Amount" />
-					<Input />
-				</div>
-				<div>
-					<Label text="Trading Platform" />
-					<SelectMultiple
-						title="[Platforms]"
-						data={['All', 'MT4', 'MT5', 'Tradologic']}
-					/>
-				</div>
-				<div>
-					<Label text="Platform Server" />
-					<SelectMultiple
-						title="[Servers]"
-						data={['All', 'Live20', 'Live21']}
-					/>
-				</div>
-				<div>
-					<Label text="Group" />
-					<SelectMultiple
-						title="[Groups]"
-						data={['All', 'usd-11', 'usd-risk']}
-					/>
-				</div>
-			</div>
+			<table>
+				<tbody>
+					<tr>
+						<td>
+							<Label text="Title" required={true} />
+							<br />
+							<Input />
+						</td>
+						<td>
+							<Label text="From" required={true} />
+							<br />
+							<Input /> : <Input />
+						</td>
+						<td>
+							<Label text="To" required={true} />
+							<br />
+							<Input /> : <Input />
+						</td>
+						<td>
+							<label>
+								<input type="checkbox" />
+								Run All Day
+							</label>
+						</td>
+						<td>
+							<Label text="Type" required={true} />
+							<br />
+							<SelectMultiple
+								title="Please select [Type]"
+								data={['All', 'Deposits', 'Withdrawals']}
+							/>
+						</td>
+						<td>
+							<Label text="Day" required={true} />
+							<br />
+							<SelectMultiple
+								title="Please select [Day]"
+								data={['All', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']}
+							/>
+						</td>
+						<td>
+							<Label text="Min Amount" />
+							<br />
+							<Input />
+						</td>
+						<td>
+							<Label text="Max Amount" />
+							<br />
+							<Input />
+						</td>
+						<td>
+							<Label text="Trading Platform" />
+							<br />
+							<SelectMultiple
+								title="[Platforms]"
+								data={['All', 'MT4', 'MT5', 'Tradologic']}
+							/>
+						</td>
+						<td>
+							<Label text="Platform Server" />
+							<br />
+							<SelectMultiple
+								title="[Servers]"
+								data={['All', 'Live20', 'Live21']}
+							/>
+						</td>
+						<td>
+							<Label text="Group" />
+							<br />
+							<SelectMultiple
+								title="[Groups]"
+								data={['All', 'usd-11', 'usd-risk']}
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 			<div>
 				<Button text="Cancel" />
 				<Button text="Add" />
